f849350e0b4baac738cc2b2d2fb045413eab0062
Pedro Sampaio <pedroigorjs@gmail.com>
2020-03-02 11:31:29 -0300

    Initial Commit: Setting Project

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './template/index.html',
+    }),
+  ],
+  devServer: {
+    open: true,
+    port: 3000,
+    hot: true,
+  },
+};
